0d869b835ce882d6a833d3f67dc46b428def22ed
zhb_golbal <807933196@qq.com>
2022-01-08 14:34:09 +0800

    My first Eclipse demo

--- TestGit/src/com/zhb/test/Demo1.java
+++ TestGit/src/com/zhb/test/Demo1.java
@@ -2,7 +2,7 @@ package com.zhb.test;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("Test Git in Eclipse!");
+		System.out.println("Test Git in Eclipse!！");
 	}
 
 }
